9c26cdbb83dc5e42ae127e49e605d553d5bb532a
DaniyarJakupov <ddr199305@gmail.com>
2018-01-26 20:16:16 +0100

    Style navbar & tabbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,9 @@
 import { Navigation } from 'react-native-navigation';
-// import Icon from 'react-native-vector-icons/Ionicons';
+import { iOSColors } from 'react-native-typography';
 
 import { registerScreens } from './screens';
 import { iconsLoaded, iconsMap } from './utils/icons';
+import { navBarStyle } from './utils/navBarStyle';
 
 registerScreens();
 
@@ -18,15 +19,33 @@ export default class Nav {
           label: 'Feed',
           screen: 'instagramApp.FeedScreen', // this is a registered name for a screen
           title: 'Feed',
-          icon: iconsMap['ios-home']
+          icon: iconsMap['ios-home'],
+          navigatorStyle: navBarStyle
         },
         {
           label: 'Explore',
           screen: 'instagramApp.ExploreScreen',
           title: 'Explore',
-          icon: iconsMap['ios-search']
+          icon: iconsMap['ios-search'],
+          navigatorStyle: navBarStyle
         }
-      ]
+      ],
+      tabsStyle: {
+        // iOS
+        tabBarButtonColor: iOSColors.midGray,
+        tabBarSelectedButtonColor: iOSColors.black,
+        tabBarBackgroundColor: iOSColors.white,
+        tabBarHideShadow: false,
+        initialTabIndex: 0
+      },
+      appStyle: {
+        // Android
+        tabBarBackgroundColor: iOSColors.white,
+        tabBarButtonColor: iOSColors.midGray,
+        tabBarSelectedButtonColor: iOSColors.black,
+        tabBarTranslucent: false,
+        tabFontFamily: 'Avenir-Medium'
+      }
     });
   }
 }
