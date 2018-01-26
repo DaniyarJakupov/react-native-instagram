cbaeface59bcc25cc6a9b9e711bfc6204eae216c
DaniyarJakupov <ddr199305@gmail.com>
2018-01-26 09:41:19 +0100

    Add ActionPanel

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 
 import Header from './Header';
+import ActionPanel from './ActionPanel';
+
+const uri =
+  'https://freestocks.org/fs/wp-content/uploads/2018/01/holly_berries_4-1024x683.jpg';
 
 class PhotoCard extends Component {
   state = {};
   render() {
     return (
-      <View style={styles.wrapper}>
-        <Header />
-        <Text>hey</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.wrapper}>
+          <Header />
+          <Image style={{ flex: 1 }} source={{ uri }} />
+          <ActionPanel />
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    minHeight: 800,
-    backgroundColor: 'lightblue',
+    height: 500,
     paddingBottom: 10
   }
 });
