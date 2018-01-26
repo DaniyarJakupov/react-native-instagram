8147cbec09a2073f5e6d0587a221d432b8a35827
DaniyarJakupov <ddr199305@gmail.com>
2018-01-26 21:24:06 +0100

    Add localhost as ip address

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,8 +1,12 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { Platform } from 'react-native';
 
-const link = new HttpLink({ uri: 'http://localhost:4000/api/graphql' }); // Connect to graphql server
+const url =
+  Platform.OS === 'ios' ? 'http://127.0.0.1:4000/api/graphql' : 'http://10.0.2.2:4000/api/graphql';
+
+const link = new HttpLink({ uri: url }); // Connect to graphql server
 const cache = new InMemoryCache(); // Create cache
 
 export const client = new ApolloClient({
