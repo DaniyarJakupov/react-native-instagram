cfab193247e311d0960e35286a461e17f37511b8
DaniyarJakupov <ddr199305@gmail.com>
2018-01-26 20:44:41 +0100

    Wrapped register screen in HOC

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,12 +2,10 @@ import { Navigation } from 'react-native-navigation';
 
 import FeedScreen from './FeedScreen';
 import ExploreScreen from './ExploreScreen';
+import WithProvider from '../components/WithProvider';
 
 // register all screens of the app (including internal ones)
 export function registerScreens() {
-  Navigation.registerComponent('instagramApp.FeedScreen', () => FeedScreen);
-  Navigation.registerComponent(
-    'instagramApp.ExploreScreen',
-    () => ExploreScreen
-  );
+  Navigation.registerComponent('instagramApp.FeedScreen', () => WithProvider(FeedScreen));
+  Navigation.registerComponent('instagramApp.ExploreScreen', () => WithProvider(ExploreScreen));
 }
